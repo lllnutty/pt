29e7812b9737978c850ccfe8492da6059cc94d86
lllnutty <lllnutty@naver.com>
2020-12-18 06:01:14 +0900

    2020-12-18

---
+++ converse/index.js
@@ -0,0 +1,15 @@
+function SliderBox2__init() {
+    $('.slider-box-2 > .slick-2').slick({
+      dots:true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnHover:false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+          centerMode:true,
+      centerPadding:0,
+      arrows:true
+    });
+  }
+  
+  SliderBox2__init();
